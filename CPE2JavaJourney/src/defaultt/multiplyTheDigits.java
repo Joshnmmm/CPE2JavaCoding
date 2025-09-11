7a31aeb8fa3f94dd787057d4f31ddf5cb41f6b4d
Josh Levin <joshhlevinlopez@gmail.com>
2025-09-11 22:09:32 +0800

    LE12 Multiply the digits of the input number like very digit of it

---
+++ CPE2JavaJourney/src/defaultt/multiplyTheDigits.java
@@ -0,0 +1,47 @@
+package defaultt;
+import java.util.Scanner;
+
+/*Multiply the Digits. Reads an integer
+between 0 and 1000. Display and multiplies
+all the digits in the integer. Assume that the
+user follows directions. (Hint: Use the                 <--- nice no error handling :))
+division and remainder operators).
+
+number structure => 564
+5 = first num 
+6 = mid num
+4 = last num
+*/
+
+public class multiplyTheDigits {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in); 
+		
+		
+		
+		System.out.printf("Enter a number between 0 and 1000:");
+		int number = s.nextInt(); 
+		s.close();
+		int lastNum, midNum, firstNum, product; 
+		lastNum = number%10; 
+		midNum = (number/10)%10; 
+		firstNum = (number/100)%10; 
+		if(firstNum == 0) {
+			product = midNum*lastNum; 
+		}
+		else if(firstNum == 0 && midNum == 0) {
+			product = lastNum; 
+		}
+		else {
+			product = firstNum * midNum * lastNum; 
+		}
+		System.out.printf("%d\n%d\n%d\n", firstNum, midNum, lastNum);
+		System.out.println("The product of all digits in " + number + " is " + product);
+
+	}
+
+}
+
+
+
